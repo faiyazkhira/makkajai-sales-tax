0a97dfd4a026c4777d00d6d4cb5d025b0cc3a6a9
Faiyaz Khira <faiyazkhira27@gmail.com>
2025-07-16 18:16:26 +0530

    Implement TaxCalculator as per requirement

--- src/main/java/com/faiyaz/salestax/TaxCalculator.java
+++ src/main/java/com/faiyaz/salestax/TaxCalculator.java
@@ -1,4 +1,19 @@
 package com.faiyaz.salestax;
 
 public class TaxCalculator {
+    public static double calculateTax(Item item){
+        double taxRate = 0.0;
+
+        if(!item.isExempt()){
+            taxRate += 0.10;
+        }
+        if(item.isImported()){
+            taxRate += 0.05;
+        }
+        return roundTax(item.getPrice() * taxRate);
+    }
+
+    public static double roundTax(double tax){
+        return Math.ceil(tax * 20.0) / 20.0;
+    }
 }
